6b6fa702da87dc064bd578a6fea1c4b1de35959a
George <george_foster2012@hotmail.co.uk>
2020-02-02 14:47:03 +0000

    Refactored bank code to use screen_search tool

--- bank.py
+++ bank.py
@@ -10,6 +10,7 @@ import tools.osrs_screen_grab as grabber
 from tools.screen_pos import Pos
 import bot
 from tools import config
+from tools import screen_search
 
 
 BANK_BOOTH = [
@@ -42,144 +43,52 @@ def bank_cycle(withdraw=[]):
     close()
 
 
-def find_close():
-    screen = grabber.grab_fullscreen()
-    _ = np.array(screen)
-    screen = np.array(screen.convert("L"))
-    template = cv2.imread("bot_ref_imgs/banking/close_bank.png", 0)
-    w, h = template.shape[::-1]
-
-    res = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
-    loc = np.where(res >= threshold)
-    if len(list(zip(*loc[::-1]))) < 1:
-        print("Couldn't find close bank... fuck.")
-        sys.exit()
-    
-    for pt in zip(*loc[::-1]):
-        if config.DEBUG:
-            cv2.rectangle(_, pt, (pt[0] + w, pt[1] + h), (25, 0, 255), 2)
-            cv2.imwrite('debug/close_bank_outcome.png', _)
-        
-        return Pos(
-            pt[0] + (w / 2),
-            pt[1] + (h / 2)
-        )
-
-
-def find_bank_inventory():
-    screen = grabber.grab_fullscreen()
-    _ = np.array(screen)
-    screen = np.array(screen.convert("L"))
-    template = cv2.imread("bot_ref_imgs/banking/bank_inventory.png", 0)
-    w, h = template.shape[::-1]
-
-    res = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
-    loc = np.where(res >= threshold)
-    if len(list(zip(*loc[::-1]))) < 1:
-        print("Couldn't find bank inventory... fuck.")
-        sys.exit()
-    
-    for pt in zip(*loc[::-1]):
-        if config.DEBUG:
-            cv2.rectangle(_, pt, (pt[0] + w, pt[1] + h), (25, 0, 255), 2)
-            cv2.imwrite('debug/bank_inventory_outcome.png', _)
-        
-        return Pos(
-            pt[0] + (w / 2),
-            pt[1] + (h / 2)
-        )
-
-
 def bank_inventory():
     # Bank inventory
-    click_pos = find_bank_inventory()
+    click_pos = screen_search.find_in_screen("bot_ref_imgs/banking/bank_inventory.png")
+    if click_pos is None:
+        print("Couldn't find bank inventory icon, something is terribly wrong.")
+        sys.exit()
     bot.click(click_pos)
 
 
 def find_item(item_ref):
-    screen = grabber.grab_fullscreen()
-    _ = np.array(screen)
-    screen = np.array(screen.convert("L"))
-    template = cv2.imread(item_ref, 0)
-    w, h = template.shape[::-1]
-
-    res = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
-    loc = np.where(res >= threshold)
-    if len(list(zip(*loc[::-1]))) < 1:
-        print("Couldn't find item in bank... fuck.")
-        sys.exit()
-    
-    for pt in zip(*loc[::-1]):
-        if config.DEBUG:
-            cv2.rectangle(_, pt, (pt[0] + w, pt[1] + h), (25, 0, 255), 2)
-            cv2.imwrite('debug/withdraw_item_outcome.png', _)
-        
-        return Pos(
-            pt[0] + (w / 2),
-            pt[1] + (h / 2)
-        )
+    return screen_search.find_in_screen(item_ref)
 
 
 def withdraw_item(item_ref):
     print("Withdrawing: %s" % item_ref)
     click_pos = find_item(item_ref)
+    if click_pos is None:
+        print("Couldn't find item [%s]. Item is not in bank view.")
+        return
     bot.click(click_pos)
 
 
 def is_bank_open():
-    screen = grabber.grab_fullscreen()
-    _ = np.array(screen)
-    screen = np.array(screen.convert("L"))
-    template = cv2.imread("bot_ref_imgs/banking/open_bank.png", 0)
-    w, h = template.shape[::-1]
-
-    res = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
-    loc = np.where(res >= threshold)
-    if len(list(zip(*loc[::-1]))) > 0:
-        return True
-    else:
-        return False
+    check = screen_search.find_in_screen("bot_ref_imgs/banking/open_bank.png")
+    return check is not None
 
 
 def find_booth():
-    screen = grabber.grab_fullscreen()
-    _ = np.array(screen)
-    screen = np.array(screen.convert("L"))
-
     booth_found = False
     for booth in BANK_BOOTH:
-        booth_template = cv2.imread(booth, 0)
-
-        w, h = booth_template.shape[::-1]
-
-        res = cv2.matchTemplate(screen, booth_template, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.8
-        loc = np.where(res >= threshold)
-        if len(list(zip(*loc[::-1]))) > 0:
-            booth_found = True
-            break
-
+       check = screen_search.find_in_screen(booth)
+       if check is not None:
+           booth_found = True
+           
     if not booth_found:
         print("Couldn't find bank booth... fuck.")
         sys.exit()
     
-    for pt in zip(*loc[::-1]):
-        if config.DEBUG:
-            cv2.rectangle(_, pt, (pt[0] + w, pt[1] + h), (25, 0, 255), 2)
-            cv2.imwrite('debug/booth_outcome.png', _)
-        
-        return Pos(
-            pt[0] + (w / 2),
-            pt[1] + (h / 2)
-        )
-    
+    return check
+  
 
 def close():
-    click_pos = find_close()
+    click_pos = screen_search.find_in_screen("bot_ref_imgs/banking/close_bank.png")
+    if click_pos is None:
+        print("Couldn't close the bank. Something is terribly wrong.")
+        sys.exit()
     bot.click(click_pos)
 
 

73d13feefa5ac834d8eae7947130ac58fa66bbd6
George <george_foster2012@hotmail.co.uk>
2020-02-09 12:34:35 +0000

    Added dedicated run script for bowstringer bot

---
+++ run_bowstringer.py
@@ -0,0 +1,45 @@
+import threading
+import time
+
+from tools import config
+from tools.session import Session
+from tools.lib import wait
+
+config.DEBUG = True
+
+
+
+# Event Manager
+from tools.event_manager import EventManager
+EM = EventManager.get_instance()
+
+# Sessions
+s = Session(0, 0)
+
+
+# Define bots
+from bots import bowstringer
+from bots import tanner
+from bots import mining
+bots = [
+    {"bot": bowstringer.Bowstringer(s), "thread": None}
+]
+
+# Run bots
+for bot in bots:
+    thread = threading.Thread(target=bot["bot"].run)
+    bot["thread"] = thread
+    thread.start()
+
+try:
+    while True:
+        EM.process_event()
+        time.sleep(.3)
+except KeyboardInterrupt:
+    pass
+
+# Exit threads and bots
+print("Shutting down bots... waiting for them to finish their loop")
+for bot in bots:
+    bot["bot"].session.exit()
+    bot["thread"].join()
